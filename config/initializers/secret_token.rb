6457aeab6275cd087ac983fef90ab58b348690cc
mihaela <pascuta.mihaela@gmail.com>
2011-10-05 14:56:24 +0300

    ceva commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = 'af96e82283ca7f0d70fa4f2ff4b258a525077fe1c5e26acf8b3e5ce8ad05d10db3c0a6648de54c2bcae4c39c6ab1b673f458940dc088d71aa19b58300e883032'
